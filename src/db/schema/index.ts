fb2c95795f58b1f1882a637491aea967818d98fa
Joan <joanitatimbinp@gmail.com>
2026-04-30 13:03:12 +0700

    feat: add export rooms and room_members in schema index

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,8 +1,11 @@
-
 import { users } from './users';
 import { messages } from './messages';
+import { rooms } from './rooms';
+import { room_members } from './room_members';
 
 export {
     users,
-    messages
+    messages,
+    rooms,
+    room_members
 };
