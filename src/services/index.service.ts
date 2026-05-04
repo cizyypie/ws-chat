cfc268ba8608b316b8b76ef75d1a70ba85eff653
Joan <joanitatimbinp@gmail.com>
2026-05-04 14:14:43 +0700

    create an index for services file

---
+++ src/services/index.service.ts
@@ -0,0 +1,7 @@
+export { AuthService } from './auth.service';
+export { ChatService } from './chat.service';
+export { RoomService } from './room.service';
+export { RoomMembersService } from './room-members.service'; // Adjust if your file is named differently (e.g., roomMembers.service)
+export { UserService } from './user.service';
+export { ViewService } from './view.service';
+export { WebSocketService } from './websocket.service';
